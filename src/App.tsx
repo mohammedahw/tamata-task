55cd91f0a2806119590886f08708f471823321f7
mohammedahw <mohammedalihw@gmail.com>
2023-02-09 15:03:55 +0300

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="App">
+      <div className="flex justify-center items-center h-screen text-2xl text-slate-600">
+        Hello World
+      </div>
+    </div>
+  );
+}
+
+export default App;
